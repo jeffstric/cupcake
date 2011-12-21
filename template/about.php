14511d59c320076e90a9815c629fc1860fa29a5b
jeffstric <jeffstricg@gmail.com>
2011-12-21 15:56:13 +0800

    增加了模板文件

---
+++ template/about.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>CupCupCake蛋糕工作坊</title>
+<meta name="keywords" content="CupCupCake,蛋糕工作坊" />
+<meta name="Description" content="CupCupCake" />
+<link href="main.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="warp">
+	<!-- start #head -->
+	<div id="head">
+    	<img src="img/logo.gif" class="logo" />
+        <ul class="menu">
+        	<li><a href="index.htm">首页</a></li>
+            <li class="on"><a href="about.htm">关于我们</a></li>
+            <li><a href="cakes.htm">蛋糕类别</a></li>
+            <li><a href="contact.htm">联系我们</a></li>
+        </ul>
+    </div>
+    <!-- end #head -->
+    <div id="navpath">
+    	<div>当前位置：首页 > 关于我们</div>
+    </div>
+    <div class="container">
+    	<ul class="banner300">
+        	<li><a href="#"><img src="img/main_p300a.gif" /></a></li>
+            <li><a href="#"><img src="img/main_p300b.gif" /></a></li>
+            <li><a href="#"><img src="img/main_p300c.gif" /></a></li>
+        </ul>
+        <div id="columnLeft">
+        	<div class="infoBoxHeading"><img src="img/lcate.gif" /></div>
+            <div class="infoBoxContents">
+            	<ul>
+                	<li><a href="#">birthday cake</a></li>
+					<li><a href="#">cup cake</a></li>
+                    <li><a href="#">kids cake</a></li>
+                    <li><a href="#">party cake</a></li>
+                    <li><a href="#">wedding cup cake</a></li>
+                    <li><a href="#">wedding tiered cake</a></li>
+                </ul>
+            </div>
+        </div>
+        <div id="bodyContent">
+        	<h2>关于我们</h2>
+        	<div class="infobox"><p>规格尺寸：200mmx300mmx250mm<br />净重：250kg<br />主要成分：鸡蛋、奶油、草莓酱、黄油等<br />圆形鲜奶水果蛋糕，时令水果装饰圆形鲜奶水果蛋糕，时令水果装饰圆形鲜奶水果蛋糕，时令水果装饰圆形鲜奶水果蛋糕，时令水果装饰圆形鲜奶水果蛋糕，时令水果装饰圆形鲜奶水果蛋糕，时令水果装饰圆形鲜奶水果蛋糕，时令水果装饰圆形鲜奶水果蛋糕，时令水果装饰圆形鲜奶水果蛋糕，时令水果装饰圆形鲜奶水果蛋糕，时令水果装饰</p></div>
+        </div>
+    </div>
+  	<!-- start #footer -->
+	<div id="footer">
+    	<div class="footerin">
+        	<div class="w400 fl">
+        	  <p><a href="index.htm">Home</a>|<a href="about.htm">About us</a>|<a href="cakes.htm">Cake</a>|<a href="contact.htm">Contact us</a></p>
+            <p>Copyright © Cup Cup Cake . All rights reserved.</p></div>
+            <div class="w400 fr"><p><a href="#">经营许可证编号：11002183号</a></p><p>Design by <a href="http://www.cnbb.com.cn" target="_blank">cnbb.com.cn</a></p>
+            </div>
+        </div>
+	</div>
+  	<!-- end #footer -->
+</div>
+</body>
+</html>
---
+++ template/cakes.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>CupCupCake蛋糕工作坊</title>
+<meta name="keywords" content="CupCupCake,蛋糕工作坊" />
+<meta name="Description" content="CupCupCake" />
+<link href="main.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="warp">
+	<!-- start #head -->
+	<div id="head">
+    	<img src="img/logo.gif" class="logo" />
+        <ul class="menu">
+        	<li><a href="index.htm">首页</a></li>
+            <li><a href="about.htm">关于我们</a></li>
+            <li class="on"><a href="cakes.htm">蛋糕类别</a></li>
+            <li><a href="contact.htm">联系我们</a></li>
+        </ul>
+    </div>
+    <!-- end #head -->
+    <div id="navpath">
+    	<div>当前位置：首页 > 蛋糕 > 杯形蛋糕</div>
+    </div>
+    <div class="container">
+    	<ul class="banner300">
+        	<li><a href="#"><img src="img/main_p300a.gif" /></a></li>
+            <li><a href="#"><img src="img/main_p300b.gif" /></a></li>
+            <li><a href="#"><img src="img/main_p300c.gif" /></a></li>
+        </ul>
+        <div id="columnLeft">
+        	<div class="infoBoxHeading"><img src="img/lcate.gif" /></div>
+            <div class="infoBoxContents">
+            	<ul>
+                	<li><a href="#">birthday cake</a></li>
+					<li><a href="#">cup cake</a></li>
+                    <li><a href="#">kids cake</a></li>
+                    <li><a href="#">party cake</a></li>
+                    <li><a href="#">wedding cup cake</a></li>
+                    <li><a href="#">wedding tiered cake</a></li>
+                </ul>
+            </div>
+        </div>
+        <div id="bodyContent">
+        	<h2>birthday cake生日蛋糕</h2>
+        	<ul>
+            	<li><div><a href="cake_info.htm"><img src="img/lspic2.jpg" /></a><h4>倾城之恋 (婚宴多层蛋糕)</h4><p>圆形鲜奶水果蛋糕，时令水果装饰</p><a href="cake_info.htm" class="more">查看详细>></a></div></li>
+                <li><div><a href="cake_info.htm"><img src="img/lspic2.jpg" /></a><h4>倾城之恋 (婚宴多层蛋糕)</h4><p>圆形鲜奶水果蛋糕，时令水果装饰</p><a href="cake_info.htm" class="more">查看详细>></a></div></li>
+                <li><div><a href="cake_info.htm"><img src="img/lspic2.jpg" /></a><h4>倾城之恋 (婚宴多层蛋糕)</h4><p>圆形鲜奶水果蛋糕，时令水果装饰</p><a href="cake_info.htm" class="more">查看详细>></a></div></li>
+                <li><div><a href="#"><img src="img/lspic2.jpg" /></a><h4>倾城之恋 (婚宴多层蛋糕)</h4><p>圆形鲜奶水果蛋糕，时令水果装饰</p><a href="#" class="more">查看详细>></a></div></li>
+                <li><div><a href="#"><img src="img/lspic2.jpg" /></a><h4>倾城之恋 (婚宴多层蛋糕)</h4><p>圆形鲜奶水果蛋糕，时令水果装饰</p><a href="#" class="more">查看详细>></a></div></li>
+                <li><div><a href="#"><img src="img/lspic2.jpg" /></a><h4>倾城之恋 (婚宴多层蛋糕)</h4><p>圆形鲜奶水果蛋糕，时令水果装饰</p><a href="#" class="more">查看详细>></a></div></li>
+                <li><div><a href="#"><img src="img/lspic2.jpg" /></a><h4>倾城之恋 (婚宴多层蛋糕)</h4><p>圆形鲜奶水果蛋糕，时令水果装饰</p><a href="#" class="more">查看详细>></a></div></li>
+                <li><div><a href="#"><img src="img/lspic2.jpg" /></a><h4>倾城之恋 (婚宴多层蛋糕)</h4><p>圆形鲜奶水果蛋糕，时令水果装饰</p><a href="#" class="more">查看详细>></a></div></li>
+                <li><div><a href="#"><img src="img/lspic2.jpg" /></a><h4>倾城之恋 (婚宴多层蛋糕)</h4><p>圆形鲜奶水果蛋糕，时令水果装饰</p><a href="#" class="more">查看详细>></a></div></li>
+            </ul>
+            <div class="pagelist"><a href="#"><b>1</b></a><a href="#">2</a><a href="#">下一页</a></div>
+        </div>
+    </div>
+  	<!-- start #footer -->
+	<div id="footer">
+    	<div class="footerin">
+        	<div class="w400 fl">
+        	  <p><a href="index.htm">Home</a>|<a href="about.htm">About us</a>|<a href="cakes.htm">Cake</a>|<a href="contact.htm">Contact us</a></p>
+            <p>Copyright © Cup Cup Cake . All rights reserved.</p></div>
+            <div class="w400 fr"><p><a href="#">经营许可证编号：11002183号</a></p><p>Design by <a href="http://www.cnbb.com.cn" target="_blank">cnbb.com.cn</a></p>
+            </div>
+        </div>
+	</div>
+  	<!-- end #footer -->
+</div>
+</body>
+</html>
