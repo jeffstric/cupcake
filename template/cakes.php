903577e2ca7e1e457da67491f6ccd863640519f9
jeffstric <jeffstricg@gmail.com>
2011-12-30 10:23:37 +0800

    修改了分页地BUG

--- template/cakes.php
+++ template/cakes.php
@@ -44,10 +44,14 @@
             </ul>
             <?if( $pagenum>0 && isset($pagelist_url)):?>
             <div class="pagelist">
+                <?if(isset($page) && $page> 0 ):?>
+                    <?fb($page)?><?fb($pagenum)?>
+                    <a href="<?=site_url($pagelist_url.($page-1) )?>">上一页</a>
+                <?endif;?>
                 <?for($i=0;$i<$pagenum;$i++):?>
                     <a href="<?=site_url($pagelist_url.$i)?>"><b><?=($i+1)?></b></a>
                 <?endfor;?>
-                <?if(isset($page) ):?> 
+                <?if(isset($page) && $page< ($pagenum-1) ):?>
                     <a href="<?=site_url($pagelist_url.($page+1) )?>">下一页</a>
                 <?endif;?>
             </div>
